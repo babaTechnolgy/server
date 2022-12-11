1791d0ec31eedfd3ad2353de85352b49af71d668
shadab khan <iamshadabkhan3@gmail.com>
2022-12-11 23:50:12 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+// import  express from "express";
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send({hi:"khan"});
+    res.send({hi:"khan"});  
 });
 
 
-const PORT = process.env.PORT || 5000 ;
-app.listen(PORT);
+// const PORT = process.env.PORT || 5000 ;
+app.listen(5000);
